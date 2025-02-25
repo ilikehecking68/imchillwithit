3c1fdd5d3630111d8d9087b5facbc4165ddfa0d5
RK <bruh>
2025-02-25 14:06:15 -0800

    afasfasfsa
    asfsa

--- src/main.cpp
+++ src/main.cpp
@@ -23,10 +23,7 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
+	pros::lcd::initialize(); // initialize brain screen
     chassis.calibrate(); // calibrate sensors
     // print position to brain screen
     pros::Task screen_task([&]() {
